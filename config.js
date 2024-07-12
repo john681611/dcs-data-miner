ed6c51311c1bee333a693b53ae4daa180355d2c9
john681611 <john681611@hotmail.com>
2024-07-12 23:47:17 +0100

    Update extract config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   MONGO_URL: "mongodb://admin:admin@127.0.0.1:27017",
   DB_NAME: "me_db",
   //FILES: "./tables/{Airbases,Airodromes,Terrains,Beacons,Radios}.lua", // Run once per theatre
-  FILES: "./tables/{Planes,Cars,Helicopters,Ships,Cargos,Weapons,Fortifications,Templates,ADEquipment,Theatres}.lua", // Run once per dcs version
+  FILES: "./tables/{Planes,Cars,Helicopters,Ships,Cargos,Weapons,Fortifications,Templates,ADEquipment,Theatres,Warehouses,Heliports,Layouts}.lua", // Run once per dcs version
   VIEWS: "./views/*.js", // Run once per dcs version
   ENVS: {
     MISSION: "http://127.0.0.1:12080/",
